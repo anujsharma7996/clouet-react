0ddfa51c5f70860e87903ba73de4aeb6a4d37696
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-12-12 23:43:02 +0530

    Added posts to store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/App";
 import { configureStore } from "./store";
 
 const store = configureStore();
-console.log("store", store);
+//console.log("store", store);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
