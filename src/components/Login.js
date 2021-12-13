dbf70b45604c5ac84ae5d276f98fd9a750436949
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-12-13 23:42:18 +0530

    User login 3

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { login } from "../actions/auth";
 
 class Login extends Component {
   constructor(props) {
@@ -29,18 +32,25 @@ class Login extends Component {
     //console.log("email ref", this.emailInputRef);
     //console.log("password ref", this.passwordInputRef);
     console.log("State", this.state);
+    const { email, password } = this.state;
+
+    if (email && password) {
+      this.props.dispatch(login(email, password));
+    }
   };
 
   render() {
+    const { error, inProgress } = this.props.auth;
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
+        {error && <div className="alert error-dailog">{error}</div>}
         <div className="field">
           <input
             type="email"
             placeholder="Email"
             required
-            //ref={this.emailInputRef}
+            // ref={this.emailInputRef}
             onChange={this.handleEmailChange}
             value={this.state.email}
           />
@@ -50,17 +60,30 @@ class Login extends Component {
             type="password"
             placeholder="Password"
             required
-            //ref={this.passwordInputRef}
+            // ref={this.passwordInputRef}
             onChange={this.handlePasswordChange}
             value={this.state.password}
           />
         </div>
         <div className="field">
-          <button onClick={this.handleFormSubmit}>Log In</button>
+          {inProgress ? (
+            <button onClick={this.handleFormSubmit} disabled={inProgress}>
+              Logging in...
+            </button>
+          ) : (
+            <button onClick={this.handleFormSubmit} disabled={inProgress}>
+              Log In
+            </button>
+          )}
         </div>
       </form>
     );
   }
 }
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+  };
+}
 
-export default Login;
+export default connect(mapStateToProps)(Login);
